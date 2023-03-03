be0535062d6c695765bcf6a6232392982b2527b3
Dhruv  Bhasin <dhruvbhasin0007@gmail.com>
2023-03-03 16:13:40 +0530

    generate password functionality

--- passwordmanager/passmanager.py/main.py
+++ passwordmanager/passmanager.py/main.py
@@ -1,21 +1,45 @@
 from tkinter import *
-import tkinter.messagebox
 from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
+
 
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    if len(website)==0 or len(password) == 0:
+        messagebox.showinfo(message="FIELDS EMPTY!\nEnter details and try again")
+    else:
 
-    with open("data.txt", "a") as data_file:
-        data_file.write(f"{website} | {email} | {password} \n")
-        website_entry.delete(0, END)
-        password_entry.delete(0, END)
+        is_ok = messagebox.askokcancel(title="wWbsite" , message= f"These are the details entered \nEmail: {email}"
+                                       f"\nPassword: {password} \nIs it okay to save")
+        if is_ok:
+            with open("data.txt", "a") as data_file:
+                data_file.write(f"{website} | {email} | {password} \n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -50,7 +74,7 @@ password_entry = Entry(width=35)
 password_entry.grid(row=3, column=1, columnspan=2)
 
 # --------------------------buttons
-generate_button_button = Button(text="Generate password", width=12)
+generate_button_button = Button(text="Generate password", width=12,command=generate_password)
 generate_button_button.grid(row=3, column=2)
 
 add_button = Button(text="Add", width=32, borderwidth=1, command=save)
